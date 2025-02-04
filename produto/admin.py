5cf91e42854978409a4475a092a3c97824e1b7cb
celiogomesdelima <celiogomesdelima@gmail.com>
2025-02-04 09:12:31 -0300

    Criação da estrutura de URLS e Views

--- produto/admin.py
+++ produto/admin.py
@@ -10,7 +10,7 @@ class VariacaoInline(admin.TabularInline):
     
 @admin.register(models.Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = 'nome',
+    list_display = ['nome', 'descricao_curta', 'get_preco_formatado', 'get_preco_promocional_formatado']
     ordering = '-id',
     list_display_links = 'nome',
     inlines = [
