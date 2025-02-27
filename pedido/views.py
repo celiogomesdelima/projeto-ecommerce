ec5bd9571ccd3c903fac0b196c3cfaba134e059c
celiogomesdelima <celiogomesdelima@gmail.com>
2025-02-27 16:55:21 -0300

    Corrigidos Bugs no resumo da Compra

--- pedido/views.py
+++ pedido/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
 
 class Pagar(View):
-    pass
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
 
 class SalvarPedido(View):
     pass
